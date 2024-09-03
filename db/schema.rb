e1c671182315d14fe838f2d57c6caccff9fbffa8
weeezik <mkihanya.33@gmail.com>
2024-09-02 21:39:04 -0700

    Use devise to add user login/setup etc.
    Followed tutorial here:
    https://www.youtube.com/watch?v=U5mU38Avdhw&list=PLS6F722u-R6IJfBrIRx3a2SBkAL4vUp2p&index=4

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_02_220923) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_03_012436) do
   create_table "musicians", force: :cascade do |t|
     t.string "genre"
     t.string "daw"
     t.integer "experience"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
