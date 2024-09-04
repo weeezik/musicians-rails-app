3564943a848526ae9cc3c63fd1dedcecbc9a43a0
weeezik <mkihanya.33@gmail.com>
2024-09-03 19:35:05 -0700

    Add navigation to login page
    Add SVG and PNG logo images for Jamii brand

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'static_pages#root'
-  resources :musicians
+  resources :musicians, except: [:edit, :show]
   get "/musicians", to: "musicians#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
